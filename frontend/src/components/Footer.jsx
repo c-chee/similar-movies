70c3042b7f86b26e20d0463f088862cbd3bc9410
Christine Chee <christinechee2000@gmail.com>
2026-01-11 11:33:28 -0500

    footer styles

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer>
-        <p>© 2026 Similar Movies App</p>
+            <p>© 2026 Similar Movies App. This project is for educational purposes only.</p>
         </footer>
     );
 }
