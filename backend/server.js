09c98ea20a9d58d94ba3e9955d25237810332cc2
Christine Chee <christinechee2000@gmail.com>
2026-01-11 13:47:25 -0500

    edits for render

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use('/api/movies', movieRoutes); // Any routes containing '/api/movies', ref
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 // === Wildcard route for React Router ===
-app.get('/*', (req, res) => {   
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
