2e146dba0dc01c83025ccb86593c2f68fc33ce59
Christine Chee <christinechee2000@gmail.com>
2026-01-11 04:54:55 -0500

    navbar styles tweek

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
         <nav className = 'navbar'>
-            <h1><Link class = 'nav-title-home' to="/">Similar Movies</Link></h1>
+            <h1><Link class = 'nav-title-home' to = '/'>Similar Movies</Link></h1>
 
             <section class = 'nav-link'>
-                <Link class = 'nav-home-link' to="/">Home</Link>
+                <Link class = 'nav-home-link' to = '/'>Home</Link>
                 <button><a href = 'https://www.themoviedb.org/?language=en-US'>TMBD &#8599;</a></button>
             </section>
             
